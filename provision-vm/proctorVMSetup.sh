d36e84011f597a21523d837c8cc08fad38b5eca5
David Tesar <david.tesar@microsoft.com>
2018-05-22 13:53:32 -0700

    Add PowerShell Core steps to Provision VM (#11)
    * Add PowerShell Core steps
    
    * remove confirmation prompt, duplicate apt source

--- provision-vm/proctorVMSetup.sh
+++ provision-vm/proctorVMSetup.sh
@@ -31,13 +31,33 @@ echo "############### Installing SQL cmd line tools ###############"
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 sudo apt-get update
-sudo apt-get install mssql-tools unixodbc-dev
-sudo apt-get update
-sudo apt-get install mssql-tools
+sudo apt-get install -y mssql-tools unixodbc-dev
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+
+#pick up changes to bash profile
+source ~/.bashrc
 
 echo "############### Pulling Openhack-tools from Github ###############"
 sudo git clone https://github.com/Azure-Samples/openhack-devops-proctor.git /home/azureuser/openhack-devops-proctor
 
-#pick up changes to bash profile
-source ~/.bashrc
+echo "############### Install Powershell Core and AzureRM modules "###############
+# https://docs.microsoft.com/en-us/powershell/scripting/setup/installing-powershell-core-on-linux?view=powershell-6#ubuntu-1604
+
+# Can likely remove because same steps as SQL cmd line tools
+# Import the public repository GPG keys
+# curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+# Register the Microsoft Ubuntu repository
+# sudo curl -o /etc/apt/sources.list.d/microsoft.list https://packages.microsoft.com/config/ubuntu/16.04/prod.list
+# Update the list of products
+# sudo apt-get update
+
+# Install PowerShell
+sudo apt-get install -y powershell
+# Start PowerShell and install AzureRm modules
+# https://docs.microsoft.com/en-us/powershell/azure/install-azurermps-maclinux?view=azurermps-6.0.0
+sudo pwsh
+Install-Module AzureRM.NetCore
+Import-Module AzureRM.Netcore
+Import-Module AzureRM.Profile.Netcore
+# Exit out of pwsh so bash can complete
+exit
