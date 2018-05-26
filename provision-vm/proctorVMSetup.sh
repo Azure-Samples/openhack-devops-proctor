d379bd0679ea691faa5686f025fa7a042dc183f1
Richard Guthrie <rguthrie@Microsoft.com>
2018-05-25 17:45:33 -0700

    testing

--- provision-vm/proctorVMSetup.sh
+++ provision-vm/proctorVMSetup.sh
@@ -37,7 +37,8 @@ curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev
-echo 'export PATH=$PATH:/opt/mssql-tools/bin' >> ~/.bashrc
+touch /home/azureuser/.bashrc
+echo 'export PATH=$PATH:/opt/mssql-tools/bin' >> /home/azureuser/.bashrc
 
 #pick up changes to bash profile
 source ~/.bashrc
