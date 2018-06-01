dd50c8a01cd0101716d29b1fbffc2a8be7a9e5a2
David Tesar <david.tesar@microsoft.com>
2018-06-01 13:09:33 -0700

    fix apiUrl case

--- provision-proctor/build_deploy_sentinel.sh
+++ provision-proctor/build_deploy_sentinel.sh
@@ -112,4 +112,4 @@ popd
 installPath="../leaderboard/sentinel/helm"
 echo -e "\nhelm install ... from: " $installPath
 
-helm install $installPath --name sentinel --set image.repository=$TAG,teams.totalNumber=$totalTeams,teams.location=$resourceGroupLocation,teams.baseName=$teamName,teams.apiUrl=$apiURL
+helm install $installPath --name sentinel --set image.repository=$TAG,teams.totalNumber=$totalTeams,teams.location=$resourceGroupLocation,teams.baseName=$teamName,teams.apiUrl=$apiUrl
