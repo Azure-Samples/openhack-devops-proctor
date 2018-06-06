934a15727615ef247e846d126d06b0d03094bec7
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-06-06 19:34:04 +0900

    Adding script for deploying FunctionApp with Azure Function app

--- provision-proctor/deploy_function.sh
+++ provision-proctor/deploy_function.sh
@@ -7,15 +7,16 @@ IFS=$'\n\t'
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
 
-usage() { echo "Usage: deploy.sh -g <resourceGroupName>  -l <location> -s <storageAccountName> -f <functionAppName> -c <cosmosDBName>" 1>&2; exit 1; }
+usage() { echo "Usage: deploy_function.sh -g <resourceGroupName>  -l <location> -s <storageAccountName> -f <functionAppName> -c <cosmosDBName> -z <zipFileName>" 1>&2; exit 1; }
 
 declare resourceGroupName=""
 declare location=""
 declare storageAccountName=""
 declare functionAppName=""
 declare cosmosDBName=""
+declare zipFileName=""
 # Initialize parameters specified from command line
-while getopts ":g:l:s:f:c:" arg; do
+while getopts ":g:l:s:f:c:z:" arg; do
     case "${arg}" in
         g)
             resourceGroupName=${OPTARG}
@@ -31,6 +32,9 @@ while getopts ":g:l:s:f:c:" arg; do
         ;;
         c)
             cosmosDBName=${OPTARG}
+        ;;
+        z)
+            zipFileName=${OPTARG}
         ;;
     esac
 done
@@ -65,6 +69,10 @@ if [[ -z "$cosmosDBName" ]]; then
     read cosmosDBName
 fi
 
+if [[ -z "$zipFileName" ]]; then
+    echo "Enter the azure function app zip file name which is used for the Function App:"
+    read zipFileName
+fi
 
 #DEBUG
 echo $resourceGroupName
@@ -72,6 +80,7 @@ echo $location
 echo $functionAppName
 echo $storageAccountName
 echo $cosmosDBName
+echo $zipFileName
 echo -e "\n"
 
 
@@ -82,7 +91,13 @@ az storage account create --name $storageAccountName --location $location  --res
 
 # TODO upload zipfile to the storage account.
 #  https://docs.microsoft.com/en-us/azure/storage/common/storage-azure-cli
+# https://backendsentinel9e99.blob.core.windows.net/public/Leaderboard1.0.0.zip
 
+storageConnectionString=$(az storage account show-connection-string -g $resourceGroupName -n $storageAccountName --query connectionString --output tsv)
+az storage container create --name container --public-access blob --connection-string $storageConnectionString
+az storage blob upload --container-name container --file $zipFileName --name $zipFileName --connection-string $storageConnectionString
+
+zipFileUrl=https://$storageAccountName.blob.core.windows.net/container/$zipFileName
 
 # Retrieve cosmosdb connection string
 cosmosdbEndpoint=$(az cosmosdb show --name $cosmosDBName  --resource-group $resourceGroupName --query documentEndpoint --output tsv)
@@ -96,6 +111,6 @@ az functionapp create --name $functionAppName --resource-group $resourceGroupNam
 # This part include Run-From-Zip deployment
 # TODO:
 # enalbe Application Insights (currently not supported on Azure CLI)
-# enable beta version (just add AppSettings, I'll do it after wake up.)
+# enable beta version (just add AppSettings, I'll do it after wake up.) FUNCTIONS_EXTENSION_VERSION=beta
 # Run-From-Zip deployment. https://github.com/Azure/app-service-announcements/issues/84
-az functionapp config appsettings set --name $functionAppName --resource-group $resourceGroupName --setting CosmosDB_Endpoint=$cosmosdbEndpoint CosmosDB_Key=$cosmosdbKey WEBSITE_RUN_FROM_ZIP=HERE_IS_ZIP_URL
+az functionapp config appsettings set --name $functionAppName --resource-group $resourceGroupName --setting CosmosDB_Endpoint=$cosmosdbEndpoint CosmosDB_Key=$cosmosdbKey WEBSITE_RUN_FROM_ZIP=$zipFileUrl FUNCTIONS_EXTENSION_VERSION=beta
