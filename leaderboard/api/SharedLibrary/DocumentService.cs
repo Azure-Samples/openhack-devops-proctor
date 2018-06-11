cd56e8f7bcad4eaa65ef68a973bb692a163f7f84
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-06-12 03:57:16 +0900

    fixes for azure functions api (#64)
    * Adding leaderboard backend url
    * fix copy file
    * fix: unmatch team/service id
    * fix the wrong service id
    * Change the helm chart for sentinel
    * Commit for sharing the current status not fixed yet
    * Change to avoid error but it still have null return

--- leaderboard/api/SharedLibrary/DocumentService.cs
+++ leaderboard/api/SharedLibrary/DocumentService.cs
@@ -46,7 +46,7 @@ namespace Services
         public async Task<IList<T>> GetAllDocumentsAsync<T>()
         {
             var query = client.CreateDocumentQuery<T>(
-                UriFactory.CreateDocumentCollectionUri(databaseId, nameof(T)))
+                UriFactory.CreateDocumentCollectionUri(databaseId, typeof(T).Name))
                 .AsEnumerable<T>();
             return query.ToList<T>();
         }
@@ -54,25 +54,39 @@ namespace Services
         public async Task<T> GetDocumentAsync<T>()
         {
             var query = client.CreateDocumentQuery<T>(
-            UriFactory.CreateDocumentCollectionUri(databaseId, nameof(T)))
+            UriFactory.CreateDocumentCollectionUri(databaseId, typeof(T).Name))
             .AsEnumerable<T>();
             return query.FirstOrDefault<T>();
         }
 
-        
-        public async Task<T> GetServiceAsync<T>(string id) where T :IDocument
+
+        public async Task<T> GetServiceAsync<T>(string id) where T : IDocument
         {
+            //var docUri = UriFactory.CreateDocumentUri(databaseId, typeof(T).Name, id);
+            //var doc = await client.ReadDocumentAsync<T>(docUri);
+            //return doc.Document;
+
+            var uri = UriFactory.CreateDocumentCollectionUri(databaseId, typeof(T).Name);
             var query = client.CreateDocumentQuery<T>(
-                UriFactory.CreateDocumentCollectionUri(databaseId, nameof(T)))
+                uri,
+                new FeedOptions { EnableCrossPartitionQuery = true })
                 .Where(f => f.Id == id)
-                .AsEnumerable<T>();  
-            return query.FirstOrDefault<T>();
+                .AsEnumerable<T>();
+            List<T> list = new List<T>();
+            foreach (var i in query)
+            {
+                list.Add(i);
+            }
+            var list2 = list;
+            return list.FirstOrDefault<T>();
+
         }
 
         public async Task<IList<T>> GetDocumentsAsync<T>(string id) where T :IDocument
         {
             var query = client.CreateDocumentQuery<T>(
-                UriFactory.CreateDocumentCollectionUri(databaseId, nameof(T)))
+                UriFactory.CreateDocumentCollectionUri(databaseId, typeof(T).Name),
+                new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true })
                 .Where(f => f.Id == id);           
             return query.ToList<T>();
         }
@@ -80,7 +94,7 @@ namespace Services
         public async Task<IList<T>> GetDocumentsAsync<T>(Func<IOrderedQueryable<T>, IQueryable<T>> queryFunction)
         {
             var queryBase = client.CreateDocumentQuery<T>(
-                UriFactory.CreateDocumentCollectionUri(databaseId, nameof(T)));
+                UriFactory.CreateDocumentCollectionUri(databaseId, typeof(T).Name));
             var query = queryFunction(queryBase);
             return query.ToList<T>();
         }
