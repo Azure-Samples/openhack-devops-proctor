d04cd1a74a1b1586b121ed5df44151fad50c7df3
Richard Guthrie <rguthrie@Microsoft.com>
2018-06-11 14:11:41 -0700

    adding echo

--- provision-team/sql_data_init.sh
+++ provision-team/sql_data_init.sh
@@ -52,12 +52,29 @@ if [[ -z "$sqlDBName" ]]; then
     read sqlDBName
 fi
 
+echo "Importing Devices"
 bcp Devices in ./data_load/Devices_export.txt -S $sqlServerFQDN -U $sqlServerUsername -P $sqlPassword -d $sqlDBName -c -t ','
+
+echo "Importing factMLOutputData"
 bcp factMLOutputData in ./data_load/factMLOutputData_export.txt -S $sqlServerFQDN -U $sqlServerUsername -P $sqlPassword -d $sqlDBName -c -t ','
+
+echo "Importing IOTHubDatas"
 bcp IOTHubDatas in ./data_load/IOTHubDatas_export.txt -S $sqlServerFQDN -U $sqlServerUsername -P $sqlPassword -d $sqlDBName -c -t ','
+
+echo "Importing POIs"
 bcp POIs in ./data_load/POIs_export.txt -S $sqlServerFQDN -U $sqlServerUsername -P $sqlPassword -d $sqlDBName -c -t ','
+
+echo "Importing TripPoints"
 bcp TripPoints in ./data_load/TripPoints_export.txt -S $sqlServerFQDN -U $sqlServerUsername -P $sqlPassword -d $sqlDBName -c -t ','
+
+echo "Importing Trips"
 bcp Trips in ./data_load/Trips_export.txt -S $sqlServerFQDN -U $sqlServerUsername -P $sqlPassword -d $sqlDBName -c -t ','
+
+echo "Importing UserProfiles"
 bcp UserProfiles in ./data_load/UserProfiles_export.txt -S $sqlServerFQDN -U $sqlServerUsername -P $sqlPassword -d $sqlDBName -c -t ','
+
+echo "Importing POISource"
 bcp POISource in ./data_load/POISource_export.txt -S $sqlServerFQDN -U $sqlServerUsername -P $sqlPassword -d $sqlDBName -c -t ','
+
+echo "Importing TripPointSource"
 bcp TripPointSource in ./data_load/TripPointSource_export.txt -S $sqlServerFQDN -U $sqlServerUsername -P $sqlPassword -d $sqlDBName -c -t ','
