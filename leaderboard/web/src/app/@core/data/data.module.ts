2e4ffd9f14f9d0bcfc2bfcd6d5c577cf0e9ee17a
David Tesar <david.tesar@microsoft.com>
2018-06-14 17:13:23 -0700

    Remove Users Service and assets

--- leaderboard/web/src/app/@core/data/data.module.ts
+++ leaderboard/web/src/app/@core/data/data.module.ts
@@ -1,11 +1,9 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { UserService } from './users.service';
 import { StateService } from './state.service';
 
 const SERVICES = [
-  UserService,
   StateService,
 ];
 
