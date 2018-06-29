b681c71f604a18c92873070fc2abb6f1c0898c23
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-06-29 14:07:21 +0900

    fix: stream analytics start return there is no content

--- provision-proctor/deploy_stream_analytics.sh
+++ provision-proctor/deploy_stream_analytics.sh
@@ -91,6 +91,4 @@ echo -e "Starting Stream Analytics Job\n"
 token=$(az account get-access-token --query accessToken --output tsv)
 subscriptionId=$(az account show --query id --output tsv)
 
-curl -X POST https://management.azure.com/subscriptions/$subscriptionId/re
-sourceGroups/$resourceGroupName/providers/Microsoft.StreamAnalytics/streamingjobs/$streamAnalyticsJobName/start?api-version=
-2015-10-01 -H "Authorization: Bearer $token" -H "Content-type: application/json" -d '{"outputStartMode" : "JobStartTime"}' -v
+curl -X POST https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.StreamAnalytics/streamingjobs/$streamAnalyticsJobName/start?api-version=2015-10-01 -H "Authorization: Bearer $token" -H "Content-type: application/json" -d '{"outputStartMode" : "JobStartTime"}' -v
