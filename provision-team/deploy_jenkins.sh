a401eb4ca97c36db6f86440f0cc95f683edde5a9
Oguz Pastirmaci <oguzp@Oguzs-MacBook-Pro.local>
2018-07-18 13:58:47 -0700

    Update opened ports

--- provision-team/deploy_jenkins.sh
+++ provision-team/deploy_jenkins.sh
@@ -64,13 +64,10 @@ fi
     az vm create --resource-group $resourceGroupName --name $jenkinsURL --admin-username jenkins --admin-password $jenkinsVMPassword --image UbuntuLTS --public-ip-address-dns-name $jenkinsURL
 
     # Open port 22
-    az vm open-port --port 80 --resource-group $resourceGroupName --name $jenkinsURL  --priority 101
-
-    # Open port 80
-    az vm open-port --port 22 --resource-group $resourceGroupName --name $jenkinsURL --priority 102
+    az vm open-port --port 22 --resource-group $resourceGroupName --name $jenkinsURL  --priority 101
 
     # Open port 8080
-    az vm open-port --port 8080 --resource-group $resourceGroupName --name $jenkinsURL --priority 103
+    az vm open-port --port 8080 --resource-group $resourceGroupName --name $jenkinsURL --priority 102
 
     # Use CustomScript extension to install.
     az vm extension set --publisher Microsoft.Azure.Extensions --version 2.0 --name CustomScript --vm-name $jenkinsURL --resource-group $resourceGroupName --settings '{"fileUris": ["https://raw.githubusercontent.com/OguzPastirmaci/openhack-jenkins-docker/master/config.sh"],"commandToExecute": "./config.sh"}'
