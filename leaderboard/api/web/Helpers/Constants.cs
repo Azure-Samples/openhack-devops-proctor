359cfdcc6cf2be08004ee2e5fbdcbca6e6c438c8
David Tesar <david.tesar@microsoft.com>
2018-08-06 13:43:25 -0700

    Add Auth + JWT which builds

---
+++ leaderboard/api/web/Helpers/Constants.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Sentinel.Helpers
+{
+    public static class Constants
+    {
+        public static class Strings
+        {
+            public static class JwtClaimIdentifiers
+            {
+                public const string Rol = "rol", Id = "id";
+            }
+
+            public static class JwtClaims
+            {
+                public const string ApiAccess = "api_access";
+            }
+        }
+    }
+}
--- leaderboard/api/web/Startup.cs
+++ leaderboard/api/web/Startup.cs
@@ -2,24 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System.Reflection;
+using AutoMapper;
+using FluentValidation.AspNetCore;
 using Newtonsoft.Json;
-using Sentinel.Utility;
+using Sentinel.Auth;
 using Sentinel.Data;
+using Sentinel.Helpers;
+using Sentinel.Models;
+using Sentinel.Utility;
+
+// using System.Net;
+// using AngularASPNETCore2WebApiAuth.Extensions;
+// using Microsoft.AspNetCore.Diagnostics;
+// using Microsoft.AspNetCore.Http;
 
 namespace Sentinel
 {
     public class Startup
     {
+
+        private const string SecretKey = "iNivDmHLpUA223sqsfhqGbMRdRj1PVkH"; // todo: get this from somewhere secure
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey));
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -30,6 +48,7 @@ namespace Sentinel
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddMvc()
                     .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
                     .AddJsonOptions(Options =>
@@ -39,11 +58,77 @@ namespace Sentinel
 
             var connectionString = SentinelConfiguration.GetConnectionString(this.Configuration);
 
+            services.AddDbContext<AppUserContext>(options =>
+                options.UseSqlServer(connectionString));
+
             services.AddDbContext<LogMessageContext>(options =>
                 options.UseSqlServer(connectionString));
 
             services.AddDbContext<LeaderboardContext>(options =>
                 options.UseSqlServer(connectionString));
+
+            // jwt wire up
+            // Get options from app settings
+            var jwtAppSettingOptions = Configuration.GetSection(nameof(JwtIssuerOptions));
+
+            // Configure JwtIssuerOptions
+            services.Configure<JwtIssuerOptions>(options =>
+            {
+                options.Issuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                options.Audience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)];
+                options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+            });
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)],
+
+                ValidateAudience = true,
+                ValidAudience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)],
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signingKey,
+
+                RequireExpirationTime = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(configureOptions =>
+            {
+                configureOptions.ClaimsIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                configureOptions.TokenValidationParameters = tokenValidationParameters;
+                configureOptions.SaveToken = true;
+            });
+
+            // api user claim policy
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiUser", policy => policy.RequireClaim(Constants.Strings.JwtClaimIdentifiers.Rol, Constants.Strings.JwtClaims.ApiAccess));
+            });
+
+            // add identity
+            var builder = services.AddIdentityCore<AppUser>(o =>
+            {
+                // configure identity options
+                o.Password.RequireDigit = false;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredLength = 6;
+            });
+            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), builder.Services);
+            builder.AddEntityFrameworkStores<AppUserContext>().AddDefaultTokenProviders();
+
+            services.AddAutoMapper();
+            services.AddMvc().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Startup>());
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
