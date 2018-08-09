18a2bb0f8c93fcbc7f04479dbd48f22fcae0a936
David Tesar <david.tesar@microsoft.com>
2018-08-09 14:19:15 -0700

    Add SQL tables for auth (#125)
    * Add SQL Auth Tables
    
    * Add auth routes readme
    
    * Fix SQL errors / warnings
    
    * add new api routes
    
    * Reorder sql

--- provision-proctor/CreateLeaderboardDb.sql
+++ provision-proctor/CreateLeaderboardDb.sql
@@ -84,3 +84,174 @@ ALTER TABLE leaderboard.dbo.Challenges ADD CONSTRAINT FK_Challenges_Team FOREIGN
 GO
 
 ALTER TABLE leaderboard.dbo.Challenges ADD CONSTRAINT FK_Challenges_ChallengeDefinition FOREIGN KEY (ChallengeDefinitionId) REFERENCES leaderboard.dbo.ChallengeDefinitions (Id)
+                                                                                                                     
+GO
+
+CREATE TABLE leaderboard.dbo.AspNetRoleClaims(
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[RoleId] [nvarchar](128) NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+ CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+CREATE TABLE leaderboard.dbo.AspNetRoles (
+	Id nvarchar(128) NOT NULL,
+	Name nvarchar(256),
+	NormalizedName nvarchar(256),
+	ConcurrencyStamp nvarchar(128)
+)
+
+GO
+
+ALTER TABLE leaderboard.dbo.AspNetRoles ADD CONSTRAINT PK_AspNetRoles PRIMARY KEY (Id)
+
+GO
+
+CREATE TABLE leaderboard.dbo.AspNetUserClaims(
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [nvarchar](128) NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+ CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+CREATE TABLE leaderboard.dbo.AspNetUserLogins(
+	[LoginProvider] [nvarchar](450) NOT NULL,
+	[ProviderKey] [nvarchar](450) NOT NULL,
+	[ProviderDisplayName] [nvarchar](max) NULL,
+	[UserId] [nvarchar](128) NOT NULL,
+ CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY CLUSTERED
+(
+	[LoginProvider] ASC,
+	[ProviderKey] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+CREATE TABLE leaderboard.dbo.AspNetUserRoles(
+	[UserId] [nvarchar](128) NOT NULL,
+	[RoleId] [nvarchar](128) NOT NULL,
+ CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY CLUSTERED
+(
+	[UserId] ASC,
+	[RoleId] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+CREATE TABLE leaderboard.dbo.AspNetUsers (
+	Id nvarchar(128) NOT NULL,
+	UserName nvarchar(256),
+	NormalizedUserName nvarchar(256),
+	Email nvarchar(256),
+	NormalizedEmail nvarchar(128),
+	EmailConfirmed bit DEFAULT ((0)) NOT NULL,
+	PasswordHash nvarchar(256),
+	SecurityStamp nvarchar(256),
+	ConcurrencyStamp nvarchar(256),
+	PhoneNumber nvarchar(50),
+	PhoneNumberConfirmed bit DEFAULT ((0)) NOT NULL,
+	TwoFactorEnabled bit DEFAULT ((0)) NOT NULL,
+	LockoutEnd datetime,
+	LockoutEnabled bit DEFAULT ((0)) NOT NULL,
+	AccessFailedCount int NOT NULL,
+	FirstName nvarchar(128),
+	Lastname nvarchar(128)
+)
+
+GO
+
+ALTER TABLE leaderboard.dbo.AspNetUsers ADD CONSTRAINT PK_AspNetUsers PRIMARY KEY (Id)
+
+GO
+
+CREATE TABLE leaderboard.dbo.AspNetUserTokens(
+	[UserId] [nvarchar](128) NOT NULL,
+	[LoginProvider] [nvarchar](450) NOT NULL,
+	[Name] [nvarchar](450) NOT NULL,
+	[Value] [nvarchar](max) NULL,
+ CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY CLUSTERED
+(
+	[UserId] ASC,
+	[LoginProvider] ASC,
+	[Name] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+ALTER TABLE leaderboard.dbo.AspNetRoleClaims  WITH CHECK ADD  CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY([RoleId])
+REFERENCES leaderboard.dbo.AspNetRoles(Id)
+ON DELETE CASCADE
+
+GO
+
+ALTER TABLE leaderboard.dbo.AspNetRoleClaims CHECK CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId]
+
+GO
+
+ALTER TABLE leaderboard.dbo.AspNetUserClaims WITH CHECK ADD  CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES leaderboard.dbo.AspNetUsers (Id)
+ON DELETE CASCADE
+
+GO
+
+ALTER TABLE leaderboard.dbo.AspNetUserClaims CHECK CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId]
+
+GO
+
+ALTER TABLE leaderboard.dbo.AspNetUserLogins  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES leaderboard.dbo.AspNetUsers(Id)
+ON DELETE CASCADE
+
+GO
+
+ALTER TABLE leaderboard.dbo.AspNetUserLogins CHECK CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId]
+
+GO
+
+ALTER TABLE leaderboard.dbo.AspNetUserRoles  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY([RoleId])
+REFERENCES leaderboard.dbo.AspNetRoles(Id)
+ON DELETE CASCADE
+
+GO
+
+ALTER TABLE leaderboard.dbo.AspNetUserRoles CHECK CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId]
+
+GO
+
+ALTER TABLE leaderboard.dbo.AspNetUserRoles  WITH CHECK ADD  CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES leaderboard.dbo.AspNetUsers ([Id])
+ON DELETE CASCADE
+
+GO
+
+ALTER TABLE leaderboard.dbo.AspNetUserRoles CHECK CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId]
+
+GO
+
+CREATE INDEX AspNetUsers_NormalizedUserName_IDX ON leaderboard.dbo.AspNetUsers (NormalizedUserName)
+CREATE INDEX AspNetUsers_NormalizedEmail_IDX ON leaderboard.dbo.AspNetUsers (NormalizedEmail)
+
+ALTER TABLE leaderboard.dbo.AspNetUserTokens WITH CHECK ADD  CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY([UserId])
+REFERENCES leaderboard.dbo.AspNetUsers ([Id])
+ON DELETE CASCADE
+
+GO
+
+ALTER TABLE leaderboard.dbo.AspNetUserTokens CHECK CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId]
+
+GO
