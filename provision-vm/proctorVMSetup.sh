5ce747e3f8f86aafc83f7c1b51af57d25242bbfe
Damien Caro <dcaro@microsoft.com>
2018-08-14 21:16:37 -0700

    WIP - Moving to cloudinit and managed disks (#134)
    * removed docker extension and used managed disk
    
    * testing packages moved to cloudinit
    
    * minor updates
    
    * minor updates
    
    * minor updates
    
    * moving kubectl and helm to cloud-init
    
    * cleaning custom script
    
    * minor updates
    
    * minor updates
    
    * removed cloud-init from ARM
    
    * updated fileUris on custom script
    
    * Added subscription login
    
    * changed env variables
    
    * updated custom script extension

--- provision-vm/proctorVMSetup.sh
+++ provision-vm/proctorVMSetup.sh
@@ -1,10 +1,26 @@
-echo "############### Installing Azure CLI v2.0.43 ###############"
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
-     sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
-sudo apt-get install -y apt-transport-https
-sudo apt-get update
+# Set Azure Credentials
+USERNAME=$1
+PASSWORD=$2
+SUBID=$3
+LOCATION=$4
+TEAMNAME=$5
+TEAMNUMBER=$6
+
+echo "############### Adding package respositories ###############"
+# Get the Microsoft signing key 
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+# Get the Docker GPG key 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+
+# Azure-cli
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ xenial main"
+# Dotnet SDK v2.1
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main"
+# Add MSSQL source 
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/ubuntu/16.04/prod xenial main"
+# Add Docker source
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 
 echo "############### Installing Helm v2.9.1 ###############"
 sudo curl -O https://storage.googleapis.com/kubernetes-helm/helm-v2.9.1-linux-amd64.tar.gz
@@ -16,30 +32,24 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.10.5/bin/l
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
-echo "############### Installing Dotnet SDK v2.1 ###############"
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+echo "############### Installing Packages ###############" 
+#  - apt-transport-https
+#  - jq
+#  - git
+#  - zip
+#  - azure-cli=2.0.31-1~xenial
+#  - mssql-tools
+#  - unixodbc-dev
+#  - dotnet-sdk-2.1.4
+#  - powershell 
+#  - docker-ce
 
-sudo apt-get update
-sudo apt-get install -y dotnet-sdk-2.1
-
-echo "############### Installing Jq ###############"
-sudo apt-get install -y jq
-
-echo "############### Installing Git ###############"
-sudo apt-get install -y git
-
-echo "############### Installing zip ###############"
-sudo apt-get install -y zip
-
-whoami > ~/output.txt
-
-echo "############### Installing SQL cmd line tools ###############"
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
-sudo apt-get update
+sudo apt-get update 
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y dotnet-sdk-2.1 jq git zip azure-cli=2.0.43-1~xenial
 sudo ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev
+sudo apt-get install -y powershell docker-ce
+
 touch /home/azureuser/.bashrc
 echo 'export PATH=$PATH:/opt/mssql-tools/bin' >> /home/azureuser/.bashrc
 
@@ -52,28 +62,41 @@ sudo install -b /home/azureuser/openhack-devops-proctor/provision-team/kvstore.s
 echo 'export KVSTORE_DIR=/home/azureuser/team_env/kvstore' >> /home/azureuser/.bashrc
 
 #pick up changes to bash profile
-source /home/azureuser/.bashrc
+#source /home/azureuser/.bashrc
 
 echo "############### Install Powershell Core and AzureRM modules ###############"
 # https://docs.microsoft.com/en-us/powershell/scripting/setup/installing-powershell-core-on-linux?view=powershell-6#ubuntu-1604
 # Install PowerShell
-sudo apt-get install -y powershell
-# Start PowerShell and install AzureRm modules
-# https://docs.microsoft.com/en-us/powershell/azure/install-azurermps-maclinux?view=azurermps-6.0.0
 
 #Change trust policy on powershell gallery to Trusted for unattended install
 sudo pwsh -command "& {Set-PSRepository -Name PSGallery -InstallationPolicy Trusted}"
-
 #Install AzureRM Modules
 sudo pwsh -command "& {Install-Module AzureRM.NetCore}"
 sudo pwsh -command "& {Import-Module AzureRM.Netcore}"
 sudo pwsh -command "& {Import-Module AzureRM.Profile.Netcore}"
 
-
-# Installing this at the end because for some reason it doesn't take effect when immediately after the AZ setup
-sudo apt-get install -y azure-cli=2.0.43-1~xenial
-
 echo azure-cli hold | sudo dpkg --set-selection
 
+#Add user to docker usergroup
+sudo usermod -aG docker azureuser
 sudo apt-get upgrade -y
 
+#Set environement variables
+export PATH=$PATH:/opt/mssql-tools/bin
+export KVSTORE_DIR=/home/azureuser/team_env/kvstore
+
+cd /home/azureuser/openhack-devops-proctor/provision-team
+
+echo "############### Azure credentials ###############"
+echo "UserName: $USERNAME"
+echo "Password: $PASSWORD"
+echo "Subscription ID: $SUBID"
+echo "Location: $LOCATION"
+echo "Team Name: $TEAMNAME"
+echo "Team number: $TEAMNUMBER"
+
+# Running the provisioning of the team environment
+az login -u $USERNAME -p $PASSWORD
+
+# Launching the team provisioning in background
+sudo PATH=$PATH:/opt/mssql-tools/bin KVSTORE_DIR=/home/azureuser/team_env/kvstore nohup ./setup.sh -i $SUBID -l $LOCATION -n $TEAMNAME -e $TEAMNUMBER >devopsohseawa$TEAMNUMBER.out &
