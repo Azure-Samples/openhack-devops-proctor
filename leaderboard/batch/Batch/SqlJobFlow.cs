ec3c72acfca422c9397a38a1c7b22b6c65d3be5b
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-08-22 09:53:12 -0700

    Update the Azure Functions with SQLs

--- leaderboard/batch/Batch/SqlJobFlow.cs
+++ leaderboard/batch/Batch/SqlJobFlow.cs
@@ -4,6 +4,7 @@ using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace Batch
 {
@@ -24,10 +25,105 @@ namespace Batch
         {
 
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            var command = new SqlCommand(queryString, connection);
+            var sw = new Stopwatch();
+            sw.Start();
+            int rowAffected = ExecuteQuery(GetCalculateTeamDowntimeSQL(), connection);
+            sw.Stop();
+            log.LogInformation($"CalculateTeamDownTime: {rowAffected} rows {sw.ElapsedMilliseconds}");
+            sw.Restart();
+            rowAffected = ExecuteQuery(GetCalculateChallangeScoreSQL(), connection);
+            sw.Stop();
+            log.LogInformation($"CalculateChallangeScoreSQL: {rowAffected} rows {sw.ElapsedMilliseconds}");
+            sw.Restart();
+            rowAffected = ExecuteQuery(GetCalculateTeamScoreSQL(), connection);
+            sw.Stop();
+            log.LogInformation($"CalculateTeamScoreSQL: {rowAffected} rows {sw.ElapsedMilliseconds}");
 
-            command.ExecuteNonQuery();
+        }
+
+        private static int ExecuteQuery(string query, SqlConnection connection)
+        {
+            var command =  new SqlCommand(query, connection);
+            return command.ExecuteNonQuery();
             
         }
+
+        private static string GetCalculateTeamDowntimeSQL()
+        {
+            return @"UPDATE TEAMS SET DOWNTIMEMINUTES = DTSUM.DTM
+    FROM  (
+        SELECT DTCALC.TEAMNAME, DTCALC.TEAMID, SUM(DOWNTIMEMINUTES) DTM
+        FROM (
+            SELECT T.TEAMNAME AS [TEAMNAME],
+                T.ID AS [TEAMID],
+                CD.[NAME] AS [CHALLENGENAME],
+                CD.[ID] AS [CHALLENGEDEFINITIONID],
+                C.ID AS [CHALLENGEID],
+                COUNT(DISTINCT L.TIMESLICE) AS DOWNTIMEMINUTES
+            FROM CHALLENGES AS C WITH (NOLOCK)
+            INNER JOIN TEAMS AS T WITH (NOLOCK)
+                ON C.TEAMID = T.ID
+            INNER JOIN CHALLENGEDEFINITIONS AS CD WITH (NOLOCK)
+                ON C.CHALLENGEDEFINITIONID = CD.ID
+            INNER JOIN LOGMESSAGES AS L WITH (NOLOCK)
+                ON T.[TEAMNAME] = L.TEAMNAME
+            WHERE L.CREATEDDATE >= C.STARTDATETIME
+                AND L.CREATEDDATE <= C.ENDDATETIME
+                AND C.ISCOMPLETED=1
+                AND CD.SCOREENABLED = 1
+            GROUP BY T.TEAMNAME, T.ID, CD.NAME, CD.[ID], C.ID
+        ) DTCALC
+        GROUP BY DTCALC.TEAMNAME, DTCALC.TEAMID) DTSUM
+    WHERE TEAMS.ID = DTSUM.TEAMID
+            ";
+        }
+
+        private static string GetCalculateChallangeScoreSQL()
+        {
+            return @"UPDATE CHALLENGES SET SCORE =
+        CASE
+            WHEN (SCORECALC.STARTINGSCORE - SCORECALC.DOWNTIMEMINUTES) < 0 THEN 0
+            ELSE SCORECALC.STARTINGSCORE - SCORECALC.DOWNTIMEMINUTES
+        END
+    FROM (
+        SELECT T.TEAMNAME AS [TEAMNAME],
+            T.ID AS [TEAMID],
+            CD.[NAME] AS [CHALLENGENAME],
+            CD.[ID] AS [CHALLENGEDEFINITIONID],
+            CD.MAXPOINTS AS [STARTINGSCORE],
+            C.ID AS [CHALLENGEID],
+            COUNT(DISTINCT L.TIMESLICE) AS DOWNTIMEMINUTES
+        FROM CHALLENGES AS C WITH (NOLOCK)
+        INNER JOIN TEAMS AS T WITH (NOLOCK)
+            ON C.TEAMID = T.ID
+        INNER JOIN CHALLENGEDEFINITIONS AS CD WITH (NOLOCK)
+            ON C.CHALLENGEDEFINITIONID = CD.ID
+        INNER JOIN LOGMESSAGES AS L WITH (NOLOCK)
+            ON T.[TEAMNAME] = L.TEAMNAME
+        WHERE L.CREATEDDATE >= C.STARTDATETIME
+            AND L.CREATEDDATE <= C.ENDDATETIME
+            AND C.ISCOMPLETED=1
+            AND CD.SCOREENABLED = 1
+        GROUP BY T.TEAMNAME, T.ID, CD.NAME, CD.ID, C.ID, CD.MAXPOINTS) SCORECALC
+    WHERE CHALLENGES.ID = SCORECALC.CHALLENGEID
+            ";
+        }
+
+        private static string GetCalculateTeamScoreSQL()
+        {
+            return @"UPDATE TEAMS SET POINTS = TEAMSCORES.TEAMSCORE
+    FROM (
+        SELECT C.TEAMID AS [TEAMID],
+            SUM(C.SCORE) AS [TEAMSCORE]
+        FROM CHALLENGES AS C
+        WHERE C.ISCOMPLETED = 1
+        GROUP BY C.TEAMID) AS TEAMSCORES
+    WHERE TEAMS.ID = TEAMSCORES.TEAMID
+            ";
+        }
+
+
     }
+
+
 }
