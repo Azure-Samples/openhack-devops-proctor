aeaf19577990827e763c89aae22434f26e1084bc
Oguz Pastirmaci <oguzp@Oguzs-MacBook-Pro.local>
2018-08-24 11:41:26 -0700

    Change variable name

--- provision-proctor/setup.sh
+++ provision-proctor/setup.sh
@@ -108,7 +108,7 @@ fi
 declare proctorName="monitoring$(randomChar;randomChar;randomChar;randomNum;)"
 declare proctorNumber="$(randomNum;randomNum;randomNum;randomNum;)"
 
-declare resourceGroupProctor="ohmonitoring${proctorNumber}rg";
+declare resourceGroupProctor="monitoring${proctorNumber}rg";
 declare registryName="${proctorName}${proctorNumber}acr"
 declare clusterName="${proctorName}${proctorNumber}aks"
 declare keyVaultName="${proctorName}${proctorNumber}kv"
