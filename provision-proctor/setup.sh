d8870ffc1a588f0893b6472fafd9589b46a5b982
Oguz Pastirmaci <oguzp@Oguzs-MacBook-Pro.local>
2018-08-24 10:58:55 -0700

    Comment out sentinel deployment

--- provision-proctor/setup.sh
+++ provision-proctor/setup.sh
@@ -228,11 +228,11 @@ bash ./build_deploy_sentinel_api.sh -b Release -r $resourceGroupProctor -t 'sent
 echo "7-Build sentinel and push to ACR (bash ./build_sentinel.sh -r $resourceGroupProctor -g $registryName -l $resourceGroupLocation -a $apiUrl)"
 bash ./build_sentinel.sh -r $resourceGroupProctor -g $registryName -l $resourceGroupLocation -a $apiUrl
 
-echo "8-Deploy sentinel to AKS"
-bash ./deploy_sentinel.sh -p ${proctorName}${proctorNumber}
-
-echo "9-Build and deploy leaderboard website to AKS  (bash ./build_deploy_web.sh -m ${proctorName}${proctorNumber} -d $dnsURL)"
-bash ./build_deploy_web.sh -m ${proctorName}${proctorNumber} -d $dnsURL
+# echo "8-Deploy sentinel to AKS"
+# bash ./deploy_sentinel.sh -p ${proctorName}${proctorNumber}
+# 
+# echo "9-Build and deploy leaderboard website to AKS  (bash ./build_deploy_web.sh -m ${proctorName}${proctorNumber} -d $dnsURL)"
+# bash ./build_deploy_web.sh -m ${proctorName}${proctorNumber} -d $dnsURL
 
 echo "10-Clean the working environment"
 bash ../provision-team/cleanup_environment.sh -t ${proctorName}${proctorNumber}
