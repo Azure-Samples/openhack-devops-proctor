5cce05619b338615d00715d7961cbf27ca4656c8
Richard Guthrie <rguthrie@Microsoft.com>
2018-08-25 08:13:21 -0700

    Fix logging

--- leaderboard/sentinel/main.go
+++ leaderboard/sentinel/main.go
@@ -5,12 +5,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"time"
 
 	"github.com/caarlos0/env"
 )
+
+////////////////////////////
+// Service Type Enum (values set in ./helm/templates/deployment.yaml)
+// 1 = poi
+// 2 = user
+// 3 = trips
+// 4 = user-java
+////////////////////////////
 
 type config struct {
 	Endpoint      string `env:"SENTINEL_ENDPOINT,required"`
@@ -20,39 +27,44 @@ type config struct {
 	APIURL        string `env:"SENTINEL_API_URL,required"`
 	Interval      int    `env:"SENTINEL_POLLING_INTERVAL" envDefault:"1"`
 	RetryDuration int    `env:"SENTINEL_RETRY_DURATION" envDefault:"1000"`
+	Debug         bool   `env:"SENTINEL_DEBUG" envDefault:false`
 }
 
 type logmsg struct {
-	TeamName    string
-	Type        string
-	CreatedDate time.Time
-	StatusCode  int
-	EndpointURI string
+	TeamName    string    //Name of Team
+	Type        string    // Service Type
+	CreatedDate time.Time // UTC DateTime
+	StatusCode  int       // Returned Status Code (0 if no response)
+	EndpointURI string    // Monitored Endpoint URI
 }
 
 func main() {
-	// Get environment variables and validate it
-	fmt.Println("Sentinel - Monitor an endpoint status for Openhack - DevOps")
-	fmt.Println("\nStarting...")
+	// Get environment variables and validate them
+	fmt.Println("Sentinel Initializing...")
 	cfg := config{}
 	err := env.Parse(&cfg)
 	if err != nil {
-		fmt.Println("Initialize Error")
+		fmt.Println("Error parsing configuration.")
 		fmt.Printf("%+v\n", err)
 		return
 	}
 
+	fmt.Println(fmt.Sprintf("Sentinel - Monitoring endpoint %s", cfg.Endpoint))
+
 	ticker := time.NewTicker(time.Duration(cfg.Interval) * time.Second)
 	for t := range ticker.C {
 		fmt.Println("Tick ...", t)
-		statusCode, err := healthCheck(&cfg)
+		statusCode, err := pingHealthCheck(&cfg)
 		if err != nil {
-			fmt.Printf("HealthCheck Error: Do you have a proper target endpoint?: %v \n", err)
+			fmt.Printf("HealthCheck Error - Check your configuration: %v \n", err)
 			statusCode = 0 // If they don't have a proper response, the statusCode becomes 0.
 		}
-		fmt.Println(fmt.Sprintf("Server: %s Status: %d", cfg.Endpoint, statusCode))
+		if cfg.Debug { // In Debug Print all ping status
+			fmt.Println(fmt.Sprintf("Server: %s Status: %d", cfg.Endpoint, statusCode))
+		}
+
 		currentTime := time.Now()
-		currentTimeRound := currentTime.Round(time.Duration(time.Second))
+		currentTimeRound := currentTime.Round(time.Duration(time.Second)) //round time to nearest second
 		logmsg := &logmsg{
 			TeamName:    cfg.TeamName,
 			Type:        cfg.ServiceType,
@@ -61,18 +73,22 @@ func main() {
 			EndpointURI: cfg.Endpoint,
 		}
 
-		// Endpoint is dead
+		// Monitored Endpoint is down
 		if statusCode != 200 {
-			res, err := report(&cfg, logmsg)
+			res, err := logHealthcheckFailed(&cfg, logmsg)
 			if err != nil {
+				//error logging message to sentinel api
 				printAPIErrorMessage(&cfg, err, res, logmsg)
 			}
-			fmt.Println("Dead! wait for recovery for 1000 ms")
+			fmt.Println(
+				fmt.Sprintf("%s down! ping retry in %d ms",
+					cfg.Endpoint,
+					time.Duration(cfg.RetryDuration)*time.Millisecond))
 			time.Sleep(time.Duration(cfg.RetryDuration) * time.Millisecond)
 		}
 	}
 
-	fmt.Println("Hello")
+	fmt.Println("Exiting...")
 }
 
 func printAPIErrorMessage(cfg *config, err error, res *http.Response, logmsg *logmsg) {
@@ -83,9 +99,14 @@ func printAPIErrorMessage(cfg *config, err error, res *http.Response, logmsg *lo
 	}
 	body, parseErr := getBody(res)
 	if parseErr != nil {
-		fmt.Println(fmt.Sprintf("Error. During sending log to the API server: API Server: %s ErrorMessage: %s, RemoteStatusCode: %d RemoteMessage: unavailable SentMessage: %s", cfg.APIURL, err.Error(), res.StatusCode, logJSON))
+		body = "unavailable"
+	}
+	if cfg.Debug {
+		fmt.Println(fmt.Sprintf("Healthcheck failed for endpoint: %s \nErrorMessage: %s \nRemoteStatusCode: %d \nRemoteMessage: %s \nSentMessage: %s",
+			cfg.APIURL, err.Error(), res.StatusCode, body, logJSON))
 	} else {
-		fmt.Println(fmt.Sprintf("Error. During sending log to the API server: API Server: %s ErrorMessage: %s, RemoteStatusCode: %d RemoteMessage: %s SentMessage: %s", cfg.APIURL, err.Error(), res.StatusCode, body, logJSON))
+		fmt.Println(fmt.Sprintf("Healthcheck failed for endpoint: %s \nErrorMessage: %s \nRemoteStatusCode: %d",
+			cfg.APIURL, err.Error(), res.StatusCode))
 	}
 }
 
@@ -99,22 +120,17 @@ func getBody(resp *http.Response) (string, error) {
 	return "", nil
 }
 
-func report(cfg *config, logmsg *logmsg) (*http.Response, error) {
+func logHealthcheckFailed(cfg *config, logmsg *logmsg) (*http.Response, error) {
 	reportJSON, err := json.Marshal(*logmsg)
 	if err != nil {
 		return nil, err
 	}
 
 	res, err := http.Post((*cfg).APIURL, "application/json", bytes.NewReader(reportJSON))
-	if res.StatusCode != 200 {
-		log.Printf("Unable to post to functions API")
-		log.Printf(string(reportJSON))
-		log.Printf(res.Status)
-	}
 	return res, err
 }
 
-func healthCheck(cfg *config) (int, error) {
+func pingHealthCheck(cfg *config) (int, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", (*cfg).Endpoint, nil)
 	req.Header.Set("Accept", "*/*")
