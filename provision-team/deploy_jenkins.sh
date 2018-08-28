d3b9073adff7f19f612bfd12f25b5cd81736f081
OguzPastirmaci <oguzp@microsoft.com>
2018-08-28 16:32:01 -0400

    Using json payload

--- provision-team/deploy_jenkins.sh
+++ provision-team/deploy_jenkins.sh
@@ -70,4 +70,5 @@ fi
     az vm open-port --port 8080 --resource-group $resourceGroupName --name $jenkinsURL --priority 102
 
     # Use CustomScript extension to install.
-    az vm extension set --publisher Microsoft.Azure.Extensions --version 2.0 --name CustomScript --vm-name $jenkinsURL --resource-group $resourceGroupName --settings '{"fileUris": ["https://raw.githubusercontent.com/Azure-Samples/openhack-devops-proctor/jenkins-security/provision-team/configure_jenkins.sh"],"commandToExecute": "./configure_jenkins.sh $jenkinsVMPassword"}'
+    # az vm extension set --publisher Microsoft.Azure.Extensions --version 2.0 --name CustomScript --vm-name $jenkinsURL --resource-group $resourceGroupName --settings '{"fileUris": ["https://raw.githubusercontent.com/Azure-Samples/openhack-devops-proctor/jenkins-security/provision-team/configure_jenkins.sh"],"commandToExecute": "./configure_jenkins.sh $jenkinsVMPassword"}'
+    az vm extension set --publisher Microsoft.Azure.Extensions --version 2.0 --name CustomScript --vm-name oguzpjentest --resource-group oguzpjentest --protected-settings "{\"fileUris\": [\"https://raw.githubusercontent.com/Azure-Samples/openhack-devops-proctor/jenkins-security/provision-team/configure_jenkins.sh\"], \"commandToExecute\": \"./configure_jenkins.sh $jenkinsVMPassword\"}"
