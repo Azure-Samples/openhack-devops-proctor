60c131384162e9dba4d822d93d9240cf3b32d6d8
Brent Samodien <BSamodien@users.noreply.github.com>
2018-08-29 13:24:35 -0400

    Base Folder Rename from TripViewer to Web

--- tripviewer/web/Startup.cs
+++ tripviewer/web/Startup.cs
@@ -57,7 +57,7 @@ namespace TripViewer
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=UserProfile}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
