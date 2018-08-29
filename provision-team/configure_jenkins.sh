2beaf8970ed3142dd9791d06ff37ed0b5fc743ce
OguzPastirmaci <oguzp@microsoft.com>
2018-08-29 14:35:03 -0400

    Remove duplicate git install

--- provision-team/configure_jenkins.sh
+++ provision-team/configure_jenkins.sh
@@ -9,7 +9,6 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce -y
-sudo apt-get install git -y
 
 # Azure CLI
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
