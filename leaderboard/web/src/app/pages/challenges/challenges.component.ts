5ec5ba1fc110f32755b9869b27146c7a02fd06da
Richard Guthrie <rguthrie@Microsoft.com>
2018-09-11 17:46:11 -0700

    Fix challenges list component

--- leaderboard/web/src/app/pages/challenges/challenges.component.ts
+++ leaderboard/web/src/app/pages/challenges/challenges.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IChallenge } from '../../shared/challenge';
+import { ChallengesService } from '../../services/challenges.service';
 @Component({
   selector: 'challenges', // tslint:disable-line
   templateUrl: './challenges.component.html',
   styleUrls: ['./challenges.component.scss'],
 })
 export class ChallengesComponent implements OnInit {
+  pageTitle = 'Manage Challenges';
 
-  constructor() { }
+  errorMessage = '';
+  _listFilter = '';
+
+  get listFilter(): string {
+    return this._listFilter;
+}
+
+set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredChallenges = this.listFilter ? this.performFilter(this.listFilter) : this.challenges;
+}
+
+filteredChallenges: IChallenge[];
+challenges: IChallenge[];
+
+performFilter(filterBy: string): IChallenge[] {
+  filterBy = filterBy.toLocaleLowerCase();
+  return this.challenges.filter((c: IChallenge) =>
+    c.team.teamName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+}
+
+  constructor(private challengeService: ChallengesService) { }
 
   ngOnInit() {
+    this.challengeService.getChallenges().subscribe(
+      challenges => {
+          this.challenges = challenges;
+          this.filteredChallenges = this.challenges;
+      },
+      error => this.errorMessage = <any> error
+  );
   }
 
 }
