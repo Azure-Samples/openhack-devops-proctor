c173422e3166ddb249834c44572923c389ff3378
Brent Samodien <BSamodien@users.noreply.github.com>
2018-09-13 15:05:05 -0700

    Adding Map View

--- tripviewer/web/Controllers/TripController.cs
+++ tripviewer/web/Controllers/TripController.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Simulator.DataObjects;
+using Simulator.DataStore.Stores;
+using TripViewer.Utility;
+
 
 namespace TripViewer.Controllers
 {
     public class TripController : Controller
     {
+        private readonly TripViewerConfiguration _envvars;
+
+        public TripController(IOptions<TripViewerConfiguration> EnvVars)
+        {
+            _envvars = EnvVars.Value ?? throw new ArgumentNullException(nameof(EnvVars));
+        }
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var teamendpoint = _envvars.TEAM_API_ENDPOINT;
+
+            //Get trips
+            TripStore t = new TripStore(teamendpoint);
+            List<Trip> trips = t.GetItemsAsync().Result;
+            //Get Last Trip
+            var last = trips.Max(trip => trip.RecordedTimeStamp);
+            var tlast = from Trip latest in trips
+                        where latest.RecordedTimeStamp == last
+                        select latest;
+            //Get TripPoints
+            TripPointStore tps = new TripPointStore(teamendpoint);
+            List<TripPoint> tripPoints = tps.GetItemsAsync(tlast.First()).Result;
+
+
+
+            return View(tripPoints);
         }
     }
 }
