679154e7f94d19afac2711c184fa71df863310f5
Damien Caro <dcaro@microsoft.com>
2018-11-02 17:07:38 -0700

    testing ()||true

--- provision-team/jenkins/install_jenkins.sh
+++ provision-team/jenkins/install_jenkins.sh
@@ -283,7 +283,7 @@ retry_until_successful run_util_script "jenkins/run-cli-command.sh" -c "version"
 #We need to install workflow-aggregator so all the options in the auth matrix are valid
 plugins=(azure-vm-agents windows-azure-storage matrix-auth workflow-aggregator azure-app-service tfs azure-acs azure-container-agents blueocean credentials-binding git ghprb kubernetes pipeline-github-lib workflow-job azure-credentials docker-plugin)
 for plugin in "${plugins[@]}"; do
-  run_util_script "jenkins/run-cli-command.sh" -c "install-plugin $plugin -deploy"
+  (run_util_script "jenkins/run-cli-command.sh" -c "install-plugin $plugin -deploy") || true 
 done
 
 #allow anonymous read access
