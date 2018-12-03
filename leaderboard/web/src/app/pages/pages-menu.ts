99b096615645cae29bf0afb06f482520259d7f38
Richard Guthrie <rguthrie@Microsoft.com>
2018-12-03 10:30:32 -0800

    Change dashboard to api-status

--- leaderboard/web/src/app/pages/pages-menu.ts
+++ leaderboard/web/src/app/pages/pages-menu.ts
@@ -2,7 +2,7 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'Dashboard',
+    title: 'API Status',
     icon: 'nb-home',
     link: '/pages/dashboard',
     home: true,
