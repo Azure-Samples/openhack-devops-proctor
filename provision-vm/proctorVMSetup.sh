fb863c7db881355b6d7d857dbb4c59dc199d0f3e
David Tesar <david.tesar@microsoft.com>
2018-12-03 22:08:50 -0800

    Update to kubectl 1.11.5

--- provision-vm/proctorVMSetup.sh
+++ provision-vm/proctorVMSetup.sh
@@ -37,7 +37,7 @@ sudo tar -zxvf helm-v2.11.0-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/helm
 
 echo "############### Installing kubectl ###############"
-curl -s -LO https://storage.googleapis.com/kubernetes-release/release/v1.11.3/bin/linux/amd64/kubectl
+curl -s -LO https://storage.googleapis.com/kubernetes-release/release/v1.11.5/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
