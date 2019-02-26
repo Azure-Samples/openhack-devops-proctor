61c8975788b7b5303cda7948fbe143c2bcc328e1
Aparna <aprao@Aparnas-MBP.guest.corp.microsoft.com>
2019-02-26 14:46:00 -0800

    Add exit code/datetime directory

--- tools/validate-deployment.sh
+++ tools/validate-deployment.sh
@@ -3,9 +3,9 @@
 # You need to provide the CSV file with all the credentials of the Azure subscriptions from the classroom management portal and a private / public SSH keypair that will be used to access the provisioning VMs
 # The error log file is where will be logged the informations regarding the failed deployments. If not provided, it defaults to error.log. 
 
-usage() { echo "Usage: getteams.sh -f <errorlog_file> -u <service principal username> -f < service principal password> -x < service principal tenant>" 1>&2; exit 1; }
+usage() { echo "Usage: validate-deployment.sh -f <errorlog_file> -u <service principal username> -x < service principal password> -t < service principal tenant>" 1>&2; exit 1; }
 
-while getopts ":f:k:p:u:x:t:" arg; do
+while getopts ":f:u:x:t:" arg; do
     case "${arg}" in
         f)
             ERROR_FILE=${OPTARG}
@@ -27,12 +27,8 @@ mkdir -p ${KEY_DIR}
 ID_RSA_PRIVATE=${KEY_DIR}/devops_openhack_key
 ID_RSA_PUBLIC=${KEY_DIR}/devops_openhack_key.pub
 ssh-keygen -t rsa -C "DevOps OpenHack" -f ${ID_RSA_PRIVATE} -P ""
-if [ $? -ne 0 ]; then
-    echo "[ERROR] Creating the ssh keypair for the proctorVM failed "
-fi
 
 echo "Keys have been generated"
-read ID_RSA_PUBLIC
 
 if [[ -z "$ERROR_FILE" ]]; then
     ERROR_FILE="./errors.log"
@@ -47,8 +43,11 @@ az login --service-principal -u $USERNAME -p $PASSWORD --tenant $TENANT
 ipaddress=$(az vm list-ip-addresses --resource-group=ProctorVMG --name=proctorVM --query "[].virtualMachine.network.publicIpAddresses[].ipAddress" -otsv)
 echo IPADDRESS:$ipaddress
 
+location=$(az group show -n ProctorVMG --query location | tr -d '"')
+date=$(date '+%d/%m/%Y_%H:%M:%S')
+
 if [[ $ipaddress =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-    teamAAD=$(echo "aprao"  | sed -e 's/.*?*@\(.*\)ops.onmicrosoft.com/\1/')
+    teamAAD=$location-$date
     echo TEAM:$teamAAD
     if [[ ! -d "$teamAAD" ]]; then
     mkdir -p $teamAAD
@@ -75,36 +74,43 @@ EOF
         sudo cp /var/lib/waagent/custom-script/download/0/stdout .;
         sudo chown azureuser:azureuser ./stdout;
 EOF
+    errorflag=true
     scp -o StrictHostKeyChecking=no -i $ID_RSA_PRIVATE azureuser@$ipaddress:./stdout ./$teamAAD/
     # Getting deployment logs 
     scp -o StrictHostKeyChecking=no -i $ID_RSA_PRIVATE azureuser@$ipaddress:/home/azureuser/openhack-devops-proctor/provision-team/teamdeploy.out ./$teamAAD/
     if [ $? -ne 0 ]; then
         echo "[ERROR] Getting teamdeploy.out file failed for subscription $subid, portal username $portalUserName, AAD $teamAAD, VM IP is $ipaddress" >> $ERROR_FILE
+        errorflag=false
     fi
 
     grep -x "poi   \[X\]" ./$teamAAD/teamdeploy.out
     if [ $? -ne 0 ]; then
-        echo "[ERROR] - Deployment of API poi has failed in subscription $subid, portal username $portalUserName, AAD $teamAAD - VM IP is $ipaddress" >> $ERROR_FILE 
+        echo "[ERROR] - Deployment of API poi has failed in subscription $subid, portal username $portalUserName, AAD $teamAAD - VM IP is $ipaddress" >> $ERROR_FILE
+        errorflag=false 
     fi
 
     grep -x "user  \[X\]" ./$teamAAD/teamdeploy.out
     if [ $? -ne 0 ]; then
         echo "[ERROR] - Deployment of API user has failed in subscription $subid, portal username $portalUserName, AAD $teamAAD - VM IP is $ipaddress" >> $ERROR_FILE
+        errorflag=false 
     fi
 
     grep -x "trips \[X\]" ./$teamAAD/teamdeploy.out
     if [ $? -ne 0 ]; then
-        echo "[ERROR] - Deployment of API trips has failed in subscription $subid, portal username $portalUserName, AAD $teamAAD - VM IP is $ipaddress" >> $ERROR_FILE 
+        echo "[ERROR] - Deployment of API trips has failed in subscription $subid, portal username $portalUserName, AAD $teamAAD - VM IP is $ipaddress" >> $ERROR_FILE
+        errorflag=false
     fi
 
     grep -x "user-java \[X\]" ./$teamAAD/teamdeploy.out
-    if [ $? -ne 0 ]; then
-        echo "[ERROR] - Deployment of API user-java has failed in subscription $subid, portal username $portalUserName, AAD $teamAAD - VM IP is $ipaddress" >> $ERROR_FILE 
+     if [ $? -ne 0 ]; then
+        echo "[ERROR] - Deployment of API user-java has failed in subscription $subid, portal username $portalUserName, AAD $teamAAD - VM IP is $ipaddress" >> $ERROR_FILE
+        errorflag=false 
     fi
     
     grep -x '############ END OF TEAM PROVISION ############' ./$teamAAD/teamdeploy.out
     if [ $? -ne 0 ]; then
-        echo "[ERROR] - Deployment of $teamAAD has failed in subscription $subid, portal username $portalUserName, AAD $teamAAD - VM IP is $ipaddress" >> $ERROR_FILE  
+        echo "[ERROR] - Deployment of $teamAAD has failed in subscription $subid, portal username $portalUserName, AAD $teamAAD - VM IP is $ipaddress" >> $ERROR_FILE 
+        errorflag=false  
     fi
 fi
         
@@ -112,6 +118,12 @@ fi
 if [[ $ZIP_FILES ]]; then
     zip -r teamfiles.zip OTA*
     echo "Data from the teams deployment are in teamfiles.zip"
+fi
+
+if [ "$errorflag"=false ]; then 
+    exit 1
+else
+    exit 0
 fi
 
 echo "######## End of validation script ########"
