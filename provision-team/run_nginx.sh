cb57f73893f847233f2abb86ca99bfcb7205d864
Damien Caro <dcaro@microsoft.com>
2019-03-01 16:27:41 -0800

    Fixed copy command in nginx

--- provision-team/run_nginx.sh
+++ provision-team/run_nginx.sh
@@ -9,7 +9,7 @@ if [[ ! -d "/home/azureuser/www" ]]; then
     mkdir -p /home/azureuser/www
 fi 
 
-cp -R /home/azureuser/openhack-devops-proctor/provision-team/nginx /home/nginx/config
+cp -R /home/azureuser/openhack-devops-proctor/provision-team/nginx/* /home/nginx/config
 
 # Add nginx to the script
 docker run --restart always -v /home/nginx/config/:/etc/nginx/conf.d/ -v /home/nginx/contents/:/usr/share/nginx/html -p 2018:80 -d nginx
