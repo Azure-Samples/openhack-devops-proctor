fdc7654c810cc9765cb2ad058ccbbebe0c1377b8
Richard Guthrie <rguthrie@Microsoft.com>
2019-03-13 14:45:06 -0700

    Change release name

--- provision-team/deploy_ingress_dns.sh
+++ provision-team/deploy_ingress_dns.sh
@@ -122,7 +122,7 @@ echo "Apiserver is: " $APISERVER
 # Prodution POD
 helm install --name team-ingress ./traefik -f traefik-values.yaml --set kubernetes.endpoint="${APISERVER}",dashboard.domain="${DASHBOARD_URL}",service.annotations."service\.beta\.kubernetes\.io/azure-dns-label-name"="${DNS_LABEL}" --debug
 #Staging POD
-helm install --name team-ingress ./traefik -f traefik-values.yaml --set kubernetes.endpoint="${APISERVER}",dashboard.domain="stage${DASHBOARD_URL}",service.annotations."service\.beta\.kubernetes\.io/azure-dns-label-name"="stage${DNS_LABEL}" --debug
+helm install --name team-ingress-stage ./traefik -f traefik-values.yaml --set kubernetes.endpoint="${APISERVER}",dashboard.domain="stage${DASHBOARD_URL}",service.annotations."service\.beta\.kubernetes\.io/azure-dns-label-name"="stage${DNS_LABEL}" --debug
 
 echo "Waiting for public IP:"
 time=0
