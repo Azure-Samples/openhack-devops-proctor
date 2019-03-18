7f883f50721e4525e20dbc82861f32620000c72e
Richard Guthrie <rguthrie@Microsoft.com>
2019-03-18 14:10:11 -0700

    Removing proctor env

--- leaderboard/web/src/app/shared/team.ts
+++
@@ -1,9 +0,0 @@
-import {IServiceStatus} from './servicestatus';
-export interface ITeam {
-    id: string;
-    teamName: string;
-    downTimeMinutes: number;
-    points: number;
-    isScoringEnabled: boolean;
-    serviceStatus: IServiceStatus[];
-}
