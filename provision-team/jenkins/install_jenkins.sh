c36a4ff175b1c1a00c2025f7622262828ca3f913
David Tesar <david.tesar@microsoft.com>
2019-06-11 11:45:31 -0700

    k8s to 1.13.5

--- provision-team/jenkins/install_jenkins.sh
+++ provision-team/jenkins/install_jenkins.sh
@@ -490,7 +490,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce
 
 # Kubectl
 echo "############### Installing kubectl ###############"
-curl -s -LO https://storage.googleapis.com/kubernetes-release/release/v1.11.8/bin/linux/amd64/kubectl
+curl -s -LO https://storage.googleapis.com/kubernetes-release/release/v1.13.7/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
