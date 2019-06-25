0fbdbf52036a1043659d4364488f982311683738
David Tesar <davete@microsoft.com>
2019-06-25 12:01:43 -0700

    Fix Openhack Deployments (#306)
    * remove duplicate login logic
    * fix tenantId logic
    * Remove labdeploy template

--- provision-vm/Docker/startup.sh
+++ provision-vm/Docker/startup.sh
@@ -26,13 +26,5 @@ echo 'export KVSTORE_DIR=/home/azureuser/team_env/kvstore' >> /home/azureuser/.b
 
 cd /home/azureuser/openhack-devops-proctor/provision-team
 
-# Running the provisioning of the team environment
-
-if [[ -z "$TENANTID" ]]; then
-    az login --username=$AZUREUSERNAME --password=$AZUREPASSWORD
-else
-    az login --service-principal --username=$AZUREUSERNAME --password=$AZUREPASSWORD --tenant=$TENANTID
-fi
-
 # Launching the team provisioning in background
 PATH=$PATH:/opt/mssql-tools/bin KVSTORE_DIR=/home/azureuser/team_env/kvstore ./setup.sh -i $SUBID -l $LOCATION -n $TEAMNAME -u "$AZUREUSERNAME" -p "$AZUREPASSWORD" -r "$RECIPIENTEMAIL" -c "$CHATCONNECTIONSTRING" -q "$CHATMESSAGEQUEUE" -t "$TENANTID" -a "$APPID" 2>&1 | tee -a /home/azureuser/logs/teamdeploy.out
