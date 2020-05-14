d5caf6ad79c2ff01283da1185d091591a946812b
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2020-05-14 14:52:36 +0200

    polling.sh updated to http2 + timestamp switch

--- resources/polling.sh
+++ resources/polling.sh
@@ -1,65 +1,75 @@
 #!/bin/bash
 
 declare -i duration=1
+declare hasTimestamp=""
 declare hasUrl=""
 declare endpoint
 
 usage() {
     cat <<END
-    polling.sh [-i] [-h] endpoint
+    polling.sh [-t] [-i] [-h] endpoint
     
     Report the health status of the endpoint
+    -t: include timestamp
     -i: include Uri for the format
     -h: help
 END
 }
 
-while getopts "ih" opt; do 
-  case $opt in 
+while getopts "tih" opt; do
+    case $opt in
+    t)
+        hasTimestamp=true
+        ;;
     i)
-      hasUrl=true
-      ;;
-    h) 
-      usage
-      exit 0
-      ;;
+        hasUrl=true
+        ;;
+    h)
+        usage
+        exit 0
+        ;;
     \?)
-     echo "Unknown option: -${OPTARG}" >&2
-     exit 1
-     ;;
-  esac
+        echo "Unknown option: -${OPTARG}" >&2
+        exit 1
+        ;;
+    esac
 done
 
-shift $((OPTIND -1))
+shift $((OPTIND - 1))
 
 if [[ $1 ]]; then
-  endpoint=$1
+    endpoint=$1
 else
-  echo "Please specify the endpoint."
-  usage
-  exit 1 
-fi 
-
+    echo "Please specify the endpoint."
+    usage
+    exit 1
+fi
 
 healthcheck() {
     declare url=$1
-    result=$(curl -i $url 2>/dev/null | grep HTTP/1.1)
+    result=$(curl -i $url 2>/dev/null | grep HTTP/2)
     echo $result
 }
 
 while [[ true ]]; do
-   result=`healthcheck $endpoint` 
-   declare status
-   if [[ -z $result ]]; then 
-      status="N/A"
-   else
-      status=${result:9:3}
-   fi 
-   timestamp=$(date "+%Y%m%d-%H%M%S")
-   if [[ -z $hasUrl ]]; then
-     echo "$timestamp | $status "
-   else
-     echo "$timestamp | $status | $endpoint " 
-   fi 
-   sleep $duration
+    result=$(healthcheck $endpoint)
+    declare status
+    if [[ -z $result ]]; then
+        status="N/A"
+    else
+        status=${result:7:3}
+    fi
+
+    if [[ -z $hasTimestamp ]]; then
+        return=${status}
+    else
+        timestamp=$(date "+%Y%m%d-%H%M%S")
+        return="${timestamp} | ${status}"
+    fi
+
+    if [[ -n $hasUrl ]]; then
+        return="${return} | ${endpoint}"
+    fi
+    echo ${return}
+    sleep $duration
 done
