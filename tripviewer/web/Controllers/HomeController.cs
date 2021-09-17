a5ca571ef0a4821014e44c5ba358daee29602606
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2021-09-17 10:04:38 +0200

    OH refresh V2 refactor (#349)
    Co-authored-by: Dariusz Porowski <Dariusz.Porowski@microsoft.com>

--- tripviewer/web/Controllers/HomeController.cs
+++ tripviewer/web/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 using TripViewer.Models;
 using TripViewer.Utility;
 
@@ -13,35 +9,25 @@ namespace TripViewer.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
         private readonly IConfiguration Configuration;
 
-        public HomeController(IConfiguration configuration)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
+            _logger = logger;
             Configuration = configuration;
         }
+
         public IActionResult Index()
         {
-            TripViewerConfiguration tv = new TripViewerConfiguration();
-
-            tv.USER_ROOT_URL = Configuration.GetValue<string>("USER_ROOT_URL");
-            tv.USER_JAVA_ROOT_URL = Configuration.GetValue<string>("USER_JAVA_ROOT_URL");
-            tv.TRIPS_ROOT_URL = Configuration.GetValue<string>("TRIPS_ROOT_URL");
-            tv.POI_ROOT_URL = Configuration.GetValue<string>("POI_ROOT_URL");
+            TripViewerConfiguration tv = new TripViewerConfiguration
+            {
+                USER_ROOT_URL = Configuration.GetValue<string>("USER_ROOT_URL"),
+                USER_JAVA_ROOT_URL = Configuration.GetValue<string>("USER_JAVA_ROOT_URL"),
+                TRIPS_ROOT_URL = Configuration.GetValue<string>("TRIPS_ROOT_URL"),
+                POI_ROOT_URL = Configuration.GetValue<string>("POI_ROOT_URL")
+            };
             return View(tv);
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
         }
 
         public IActionResult Privacy()
--- tripviewer/web/Utility/TripViewerConfiguration.cs
+++ tripviewer/web/Utility/TripViewerConfiguration.cs
@@ -1,16 +1,13 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 
 namespace TripViewer.Utility
 {
-
     public class TripViewerConfiguration
     {
-        public string BING_MAPS_KEY { get;set; }
+        public string BING_MAPS_KEY { get; set; }
         public string USER_ROOT_URL { get; set; }
         public string USER_JAVA_ROOT_URL { get; set; }
         public string TRIPS_ROOT_URL { get; set; }
