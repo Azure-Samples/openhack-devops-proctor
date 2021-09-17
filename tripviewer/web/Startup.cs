096a98c1dce4c14e4053d5fb8f6b146e5ee0a7c3
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2021-09-17 19:25:45 +0200

    v2 updates (#352)
    * OH refresh V2 refactor (#349)
    
    Co-authored-by: Dariusz Porowski <Dariusz.Porowski@microsoft.com>
    
    * Fixing dotnet 2.1 call (#343)
    
    * Fixing dotnet 2.1 call
    
    * Correcting for polling of HTTP/2 sites
    
    * fixes (#350)
    
    Co-authored-by: Dariusz Porowski <Dariusz.Porowski@microsoft.com>
    
    * url fix (#351)
    
    Co-authored-by: Dariusz Porowski <Dariusz.Porowski@microsoft.com>
    
    Co-authored-by: Dariusz Porowski <Dariusz.Porowski@microsoft.com>
    Co-authored-by: Kit Skinner <42873266+KitSkin@users.noreply.github.com>

--- tripviewer/web/Startup.cs
+++ tripviewer/web/Startup.cs
@@ -1,36 +1,34 @@
-﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using TripViewer.Utility;
+using Microsoft.Extensions.Hosting;
 
 namespace TripViewer
 {
     public class Startup
     {
-        public IHostingEnvironment HostingEnvironment { get; private set; }
-        public IConfiguration Configuration { get; private set; }
-
-        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            // Set up configuration sources.
-            this.HostingEnvironment = env;
-            this.Configuration = configuration;
+            Configuration = configuration;
         }
 
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddControllersWithViews();
             services.AddHttpClient();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -39,16 +37,21 @@ namespace TripViewer
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
