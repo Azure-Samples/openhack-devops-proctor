a233996977c59d98c1cc010d066741595df60db0
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2021-09-20 17:44:23 +0200

    .

--- provision-team/terraform-state/locals.defaults.tf
+++ provision-team/terraform-state/locals.defaults.tf
@@ -3,7 +3,5 @@ locals {
   _default = {
     location                  = "westus2"
     ado_project_name          = "openhack"
-    # ado_org_service_url       = ""
-    # ado_personal_access_token = ""
   }
 }
