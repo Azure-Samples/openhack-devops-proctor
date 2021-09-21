ee90a8dbd5f65a2b83a9b099266a2d02a26f301f
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2021-09-21 17:08:22 +0200

    .

--- provision-team/terraform/providers.tf
+++ provision-team/terraform/providers.tf
@@ -5,6 +5,8 @@ terraform {
       version = "2.77.0"
     }
   }
+  backend "azurerm" {
+  }
   # backend "azurerm" {
   #   resource_group_name  = var.resource_group_name
   #   storage_account_name = var.storage_account_name
