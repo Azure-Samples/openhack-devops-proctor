3e64cc6b8c55c9650bba204274ca8bca079d15e6
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2021-09-21 19:38:46 +0200

    .

--- provision-team/terraform/infra.tf
+++ provision-team/terraform/infra.tf
@@ -15,11 +15,9 @@ resource "azurerm_key_vault" "key_vault" {
   name                        = local.key_vault_name
   location                    = local.location
   resource_group_name         = local.resource_group_name
-  enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
-
   sku_name = "standard"
 }
 
