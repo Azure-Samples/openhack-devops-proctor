941eb4db5aa954cb228734d58c73b0e57e011813
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2021-09-24 16:21:10 +0200

    .

--- provision-team/terraform-state-gh/main.tf
+++ provision-team/terraform-state-gh/main.tf
@@ -41,54 +41,42 @@ data "github_repository" "repo" {
   name = local.gh_repo_name
 }
 
-# data "github_actions_public_key" "actions_public_key" {
-#   repository = data.github_repository.repo.full_name
-# }
-
-# resource "github_actions_secret" "actions_secret_resources_prefix" {
-#   repository      = data.github_repository.repo.full_name
-#   secret_name     = "RESOURCES_PREFIX"
-#   plaintext_value = local.resources_prefix
-# }
-
-# resource "github_actions_secret" "actions_secret_location" {
-#   repository      = data.github_repository.repo.full_name
-#   secret_name     = "LOCATION"
-#   plaintext_value = azurerm_resource_group.resource_group.location
-# }
-
-# resource "github_actions_secret" "actions_secret_resource_group_name" {
-#   repository      = data.github_repository.repo.full_name
-#   secret_name     = "TFSTATE_RESOURCES_GROUP_NAME"
-#   plaintext_value = azurerm_resource_group.resource_group.name
-# }
-
-# resource "github_actions_secret" "actions_secret_storage_account_name" {
-#   repository      = data.github_repository.repo.full_name
-#   secret_name     = "TFSTATE_STORAGE_ACCOUNT_NAME"
-#   plaintext_value = azurerm_storage_account.storage_account.name
-# }
-
-# resource "github_actions_secret" "actions_secret_storage_container_name" {
-#   repository      = data.github_repository.repo.full_name
-#   secret_name     = "TFSTATE_STORAGE_CONTAINER_NAME"
-#   plaintext_value = azurerm_storage_container.storage_container.name
-# }
-
-# resource "github_actions_secret" "actions_secret_tfstate_key" {
-#   repository      = data.github_repository.repo.full_name
-#   secret_name     = "TFSTATE_KEY"
-#   plaintext_value = local.tfstate_key
-# }
-
-resource "github_repository_environment" "repository_environment" {
-  repository       = data.github_repository.repo.name
-  environment      = "default"
+data "github_actions_public_key" "actions_public_key" {
+  repository = data.github_repository.repo.name
 }
 
-resource "github_actions_environment_secret" "test_secret" {
-  repository       = data.github_repository.repo.name
-  environment      = github_repository_environment.repository_environment.environment
-  secret_name      = "test_secret_name"
-  plaintext_value  = "test"
+resource "github_actions_secret" "actions_secret_resources_prefix" {
+  repository      = data.github_repository.repo.name
+  secret_name     = "RESOURCES_PREFIX"
+  plaintext_value = local.resources_prefix
+}
+
+resource "github_actions_secret" "actions_secret_location" {
+  repository      = data.github_repository.repo.name
+  secret_name     = "LOCATION"
+  plaintext_value = azurerm_resource_group.resource_group.location
+}
+
+resource "github_actions_secret" "actions_secret_resource_group_name" {
+  repository      = data.github_repository.repo.name
+  secret_name     = "TFSTATE_RESOURCES_GROUP_NAME"
+  plaintext_value = azurerm_resource_group.resource_group.name
+}
+
+resource "github_actions_secret" "actions_secret_storage_account_name" {
+  repository      = data.github_repository.repo.name
+  secret_name     = "TFSTATE_STORAGE_ACCOUNT_NAME"
+  plaintext_value = azurerm_storage_account.storage_account.name
+}
+
+resource "github_actions_secret" "actions_secret_storage_container_name" {
+  repository      = data.github_repository.repo.name
+  secret_name     = "TFSTATE_STORAGE_CONTAINER_NAME"
+  plaintext_value = azurerm_storage_container.storage_container.name
+}
+
+resource "github_actions_secret" "actions_secret_tfstate_key" {
+  repository      = data.github_repository.repo.name
+  secret_name     = "TFSTATE_KEY"
+  plaintext_value = local.tfstate_key
 }
